f669ef942ba0f83163e1a9b621c1641912e7fee2
Yafei Yuan <94538486+YafeiYuan@users.noreply.github.com>
2021-11-20 11:56:45 +0800

    Update VGG16+SelfAttention.py

--- VGG16+SelfAttention.py
+++ VGG16+SelfAttention.py
@@ -122,3 +122,91 @@ class VGG16(nn.Module):
         feature = feature.view(x.size(0), -1)  # reshape x变成[batch_size,channels*width*height]
         result = self.classfication(feature)
         return result
+    
+# 这种重写方式失去了 预训练部分 但是可以下载网络中vgg16预训练好的模型并加载进来也是可以的。
+class VGG16(nn.Module):
+    def __init__(self, nums, pretrained):
+        super(VGG16, self).__init__()
+        self.nums = nums
+        vgg = []
+
+        if pretrained: # 传入预训练后vgg16模型URL
+            state_dict = load_state_dict_from_url(model_urls[arch],
+                                                  progress=progress)
+            vgg = model.load_state_dict(state_dict)
+
+        # 第一个卷积部分
+        # 112, 112, 64
+        vgg.append(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # 第二个卷积部分
+        # 56, 56, 128
+        vgg.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # 第三个卷积部分
+        # 28, 28, 256
+        vgg.append(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # 第四个卷积部分
+        # 14, 14, 512
+        vgg.append(nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # 第五个卷积部分
+        # 7, 7, 512
+        vgg.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        vgg.append(nn.ReLU(inplace=True))
+        vgg.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # 将每一个模块按照他们的顺序送入到nn.Sequential中,输入要么事orderdict,要么事一系列的模型，遇到上述的list，必须用*号进行转化
+        self.main = nn.Sequential(*vgg)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(7,7))
+
+        # 全连接层
+        classfication = []
+        # in_features四维张量变成二维[batch_size,channels,width,height]变成[batch_size,channels*width*height]
+        classfication.append(nn.Linear(in_features=512 * 7 * 7, out_features=4096))  # 输出4096个神经元，参数变成512*7*7*4096+bias(4096)个
+        classfication.append(nn.ReLU(inplace=True))
+        classfication.append(nn.Dropout(p=0.5))
+        # classfication.append(nn.Linear(in_features=4096, out_features=4096))
+        # classfication.append(nn.ReLU(inplace=True))
+        # classfication.append(nn.Dropout(p=0.5))
+        classfication.append(nn.Linear(in_features=4096, out_features=1000))
+        classfication.append(nn.ReLU(inplace=True))
+        classfication.append(nn.Dropout(p=0.5))
+        classfication.append(nn.Linear(in_features=1000, out_features=self.nums))
+
+        self.classfication = nn.Sequential(*classfication)
+
+    def forward(self, x):
+        feature = self.main(x)  # 输入张量x
+        # print(feature.shape)
+        feature = self.avgpool(feature)
+        feature = feature.view(x.size(0), -1)  # reshape x变成[batch_size,channels*width*height]
+        result = self.classfication(feature)
+        return result
+
